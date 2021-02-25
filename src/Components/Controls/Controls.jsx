b64780a8a4612de2ab65571966b8708ae69fa90d
arkadiibasovych <arkadiybasovich@gmai.com>
2021-02-26 00:29:03 +0200

    add step 2

--- src/Components/Controls/Controls.jsx
+++ src/Components/Controls/Controls.jsx
@@ -10,6 +10,16 @@ class Controls extends React.Component {
     bad: 0,
   };
 
+  countTotalFeedback = (good, neutral, bad) => {
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = (good, neutral, bad) => {
+    const total = good + neutral + bad;
+    const percent = (good * 100) / total;
+    return Math.trunc(percent);
+  };
+
   handleGoodIncrement = () => {
     this.setState(prevState => {
       return {
@@ -47,6 +57,16 @@ class Controls extends React.Component {
           good={this.state.good}
           neutral={this.state.neutral}
           bad={this.state.bad}
+          total={this.countTotalFeedback(
+            this.state.good,
+            this.state.neutral,
+            this.state.bad,
+          )}
+          percantage={this.countPositiveFeedbackPercentage(
+            this.state.good,
+            this.state.neutral,
+            this.state.bad,
+          )}
         />
       </>
     );
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import s from './Statistics.module.css';
 
-const Statistics = ({ good = 0, neutral = 0, bad = 0 }) => {
+const Statistics = ({
+  good = 0,
+  neutral = 0,
+  bad = 0,
+  total = 0,
+  percantage = 0,
+}) => {
   return (
     <div className={s.Statistics}>
       <h2 className={s.head}>Statistics</h2>
@@ -9,6 +15,8 @@ const Statistics = ({ good = 0, neutral = 0, bad = 0 }) => {
         <li className={s.feedback_item}>Good: {good}</li>
         <li className={s.feedback_item}>Neutral: {neutral}</li>
         <li className={s.feedback_item}>Bad: {bad}</li>
+        <li className={s.feedback_item}>Total: {total}</li>
+        <li className={s.feedback_item}>Positive feedback: {percantage}</li>
       </ul>
     </div>
   );
